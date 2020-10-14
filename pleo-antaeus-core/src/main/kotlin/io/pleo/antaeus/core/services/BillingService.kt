e308252be7d952b41197154f905ecf08c4972686
Tottis1986 <pkonidis86@gmail.com>
2020-10-14 20:41:10 +0300

    5: Fix billing date to take into account weekends
    - getNextBillingDate is fixed to take into account weekends so that,
    when Saturday or Sunday, the payments should go through the following
    "Monday"

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -7,16 +7,22 @@ import io.pleo.antaeus.models.Invoice
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-
     internal fun getNextBillingDate() : Date {
         val cal = Calendar.getInstance().also {
-            it.set(Calendar.MONTH, Calendar.MONTH+1)
-            it.set(Calendar.HOUR_OF_DAY, 8)
+            it.set(Calendar.MONTH, it.get(Calendar.MONTH)+1) // next Month
             it.set(Calendar.DAY_OF_MONTH, it.getActualMinimum(Calendar.DAY_OF_MONTH))
+            it.set(Calendar.HOUR_OF_DAY, 12)
+        }
+
+        // Check if its on a weekend
+        if (Calendar.SATURDAY == cal.get(Calendar.DAY_OF_WEEK)) {
+            cal.add(Calendar.DAY_OF_WEEK, 2);
+        } else if (Calendar.SUNDAY == cal.get(Calendar.DAY_OF_WEEK)) {
+            cal.add(Calendar.DAY_OF_WEEK, 1);
         }
         return cal.getTime();
     }
-    
+
     fun schedulePays() : String {
         return "Payments scheduled for: " + getNextBillingDate()
     }
