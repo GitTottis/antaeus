20157586e6ab74c2bf99cec0d569a18dd6521d57
Tottis1986 <pkonidis86@gmail.com>
2020-10-15 15:00:07 +0300

    6: Set Timer task to run on the 1st of every month
    - Introduces invoiceServiceProvider and CustomerProvider in the Billing
    service to have access to their respective getters.
    - Introduces a TimerTask in order to run the billing on the first of
    every month

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,12 +1,24 @@
 package io.pleo.antaeus.core.services
 
 import java.util.*
+import java.util.Queue
+import java.util.LinkedList
+
+import kotlin.concurrent.schedule
+
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.core.services.CustomerService
+import io.pleo.antaeus.core.services.InvoiceService
 import io.pleo.antaeus.models.Invoice
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val customerService: CustomerService,
+    private val invoiceService: InvoiceService
 ) {
+    val invoices: Queue<Invoice> = LinkedList<Invoice>()
+    var timer: Timer = Timer(true)
+
     internal fun getNextBillingDate() : Date {
         val cal = Calendar.getInstance().also {
             it.set(Calendar.MONTH, it.get(Calendar.MONTH)+1) // next Month
@@ -22,8 +34,32 @@ class BillingService(
         }
         return cal.getTime();
     }
-
-    fun schedulePays() : String {
-        return "Payments scheduled for: " + getNextBillingDate()
+    
+    internal fun processPayments() {
+            try {
+                println("Next Billing Round will run on: " + getNextBillingDate())
+            } finally {
+                setNextPaymentsTimer()
+            }
     }
+
+    internal fun setNextPaymentsTimer() {
+        // Create new Timer
+        timer = Timer("PayTimerThread", true)
+        // timer.schedule(3000) {processPayments()}
+        timer.schedule(getNextBillingDate()) {processPayments()}
+    }
+
+    fun schedulePays() : Queue<Invoice>  {
+        // Load Invoices
+        if (invoices.isEmpty()) {
+            val invoiceIterator = invoiceService.fetchAll().iterator()
+            while(invoiceIterator.hasNext()) {
+                invoices.add(invoiceIterator.next())
+            }
+        }
+        setNextPaymentsTimer()
+        return invoices
+    }
+
 }
